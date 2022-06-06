2f293d08c483ca3d90ec908c1fc53d7c8b547813
Laura Nunes <lauranunes@ibm.com>
2022-06-06 18:46:35 -0300

    atualização de exemplos de dados 2

--- TiposDeDadosBasicos_CSharp/TiposDeDadosBasicos_CSharp/Program.cs
+++ TiposDeDadosBasicos_CSharp/TiposDeDadosBasicos_CSharp/Program.cs
@@ -1,6 +1,24 @@
 ﻿sbyte x = -128;
 byte y = 255;
+bool verificacao = true;
+char sexo = 'F';
+float altura = 1.73f;
+object obj1 = "Laura";
+object obj2 = 23;
+
+sbyte obj3 = sbyte.MinValue;
+sbyte obj4 = sbyte.MaxValue;
+decimal maximo = decimal.MaxValue;
 
 y += 2;
+
 Console.WriteLine(x);
 Console.WriteLine(y);
+Console.WriteLine(verificacao);
+Console.WriteLine(sexo);
+Console.WriteLine(altura);
+Console.WriteLine(obj1);
+Console.WriteLine(obj2);
+Console.WriteLine(obj3);
+Console.WriteLine(obj4);
+Console.WriteLine(maximo);
